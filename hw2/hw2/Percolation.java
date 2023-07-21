769b6b27ea41d09c379225b00b9ef287ee1008ac
teddynight <tanyuliang2@gmail.com>
2023-07-21 11:36:21 +0800

    hw2: initial version for percolation

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -2,6 +2,146 @@ package hw2;
 
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
-public class Percolation {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Percolation {
+    private int N;
+    private WeightedQuickUnionUF grids;
+    int openSite;
+    /**
+     * create N-by-N grid, with all sites initially blocked
+     * @param N
+     */
+    public Percolation(int N) {
+        if (N <= 0) {
+            throw new IllegalArgumentException();
+        }
+        this.N = N;
+        grids = new WeightedQuickUnionUF(N * N + 3);
+        openSite = 0;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                int site = xyTo1D(i, j);
+                // mark the site blocked
+                grids.union(site, N * N + 2);
+            }
+        }
+    }
+
+    /**
+     * return the corresponding #
+     * @param r
+     * @param c
+     * @return
+     */
+    private int xyTo1D(int r, int c) {
+        return r * 5 + c;
+    }
+
+    /**
+     * open the site (row, col) if it is not open already
+     * @param row
+     * @param col
+     */
+    public void open(int row, int col) {
+        if (!isValidSite(row, col)) {
+            throw new IndexOutOfBoundsException();
+        }
+        int site = xyTo1D(row, col);
+        // open already
+        if (grids.connected(site, N * N)) {
+            return;
+        }
+        openSite++;
+        grids.union(site, site);
+        if (row == 0) {
+            // virtual top site
+            grids.union(site, N * N);
+        }
+        if (row == N - 1) {
+            // virtual bottom site
+            grids.union(site, N * N + 1);
+        }
+        for (int neighbor: neighborSite(row, col)) {
+            // not connected
+            if (grids.find(neighbor) != N * N + 2) {
+                grids.union(site, neighbor);
+            }
+        }
+    }
+
+    /**
+     * return all possible valid neighbors
+     * @param row
+     * @param col
+     * @return
+     */
+    private List<Integer> neighborSite(int row, int col) {
+        final int[][] neighbors = {{row - 1, col}, {row + 1, col}, {row, col + 1}, {row, col - 1}};
+        List<Integer> ret = new ArrayList<>();
+        for (int[] neighbor: neighbors) {
+            if (isValidSite(neighbor[0], neighbor[1])) {
+                ret.add(xyTo1D(neighbor[0], neighbor[1]));
+            }
+        }
+        return ret;
+    }
+
+    /**
+     * is the site (row, col) open?
+     * @param row
+     * @param col
+     * @return
+     */
+    public boolean isOpen(int row, int col) {
+        if (!isValidSite(row, col)) {
+            throw new IndexOutOfBoundsException();
+        }
+        return true;
+    }
+
+    /**
+     * is the site (row, col) full?
+     * @param row
+     * @param col
+     * @return
+     */
+    public boolean isFull(int row, int col) {
+        if (!isValidSite(row, col)) {
+            throw new IndexOutOfBoundsException();
+        }
+        int site = xyTo1D(row, col);
+        return grids.connected(site, N * N);
+    }
+
+    /**
+     * number of open sites
+     * @return
+     */
+    public int numberOfOpenSites() {
+        return openSite;
+    }
+
+    /**
+     * does the system percolate?
+     * @return
+     */
+    public boolean percolates() {
+        return grids.connected(N * N, N * N + 1);
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+    private boolean isValidSite(int row, int col) {
+        if (row < 0 || row >= N) {
+            return false;
+        }
+        if (col < 0 || col >= N) {
+            return false;
+        }
+        return true;
+    }
 }
