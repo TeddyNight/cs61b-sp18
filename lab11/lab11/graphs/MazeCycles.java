6771e1943a476ed783145d0daae40636a21ae3c4
teddynight <tanyuliang2@gmail.com>
2023-07-27 18:21:00 +0800

    lab11: implemented MazeCycles

--- lab11/lab11/graphs/MazeCycles.java
+++ lab11/lab11/graphs/MazeCycles.java
@@ -1,5 +1,7 @@
 package lab11.graphs;
 
+import edu.princeton.cs.algs4.Stack;
+
 /**
  *  @author Josh Hug
  */
@@ -9,16 +11,47 @@ public class MazeCycles extends MazeExplorer {
     public int[] edgeTo;
     public boolean[] marked;
     */
+    private Maze maze;
+    private int[] parent;
+    private final int s = 0;
 
     public MazeCycles(Maze m) {
         super(m);
+        maze = m;
+        parent = new int[m.V()];
+        parent[s] = s;
     }
 
     @Override
     public void solve() {
-        // TODO: Your code here!
+        Stack<Integer> toVisit = new Stack<>();
+        toVisit.push(s);
+        while (!toVisit.isEmpty()) {
+            int v = toVisit.pop();
+            marked[v] = true;
+            announce();
+            for (int w: maze.adj(v)) {
+                if (!marked[w]) {
+                    toVisit.push(w);
+                    parent[w] = v;
+                }
+                if (marked[w] && w != parent[v]) {
+                    connectCycle(v, w);
+                    announce();
+                    return;
+                }
+            }
+        }
     }
 
-    // Helper methods go here
+    private void connectCycle(int end, int start) {
+        edgeTo[start] = end;
+        int x = end;
+        while (x != start) {
+            edgeTo[x] = parent[x];
+            x = parent[x];
+        }
+    }
+
 }
 
