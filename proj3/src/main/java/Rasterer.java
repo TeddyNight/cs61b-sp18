827304ddc26b77bc0aded8f610b20da0a4ee2103
teddynight <tanyuliang2@gmail.com>
2023-07-30 16:51:19 +0800

    proj3: finish Rasterer, implemented getGrid() & getRasters

--- proj3/src/main/java/Rasterer.java
+++ proj3/src/main/java/Rasterer.java
@@ -50,12 +50,86 @@ public class Rasterer {
         }
         double lrlon = params.get("lrlon");
         double ullon = params.get("ullon");
+        double lrlat = params.get("lrlat");
+        double ullat = params.get("ullat");
         double w = params.get("w");
         double h = params.get("h");
         double boxLonDPP = calcLonDPP(lrlon, ullon, w);
         int depth = getDepth(boxLonDPP, w, h);
+        double rasterUlLon = getRasterULLON(ullon, depth);
+        double rasterLrLon = getRasterLRLON(lrlon, depth);
+        double rasterUlLat = getRasterULLAT(ullat, depth);
+        double rasterLrLat = getRasterLRLAT(lrlat, depth);
+        String[][] renderGrid = getGrid(rasterUlLat, rasterLrLat,
+                rasterUlLon, rasterLrLon, depth);
+        results.put("render_grid", renderGrid);
+        results.put("raster_ul_lon", rasterUlLon);
+        results.put("raster_lr_lon", rasterLrLon);
+        results.put("raster_ul_lat", rasterUlLat);
+        results.put("raster_lr_lat", rasterLrLat);
         results.put("depth", depth);
+        results.put("query_success", true);
         return results;
+    }
+
+    private String[][] getGrid(double ullat, double lrlat,
+                               double ullon, double lrron, int depth) {
+        double N = Math.pow(2, depth);
+        double latOffset = (MapServer.ROOT_ULLAT - MapServer.ROOT_LRLAT) / N;
+        double lonOffset = (MapServer.ROOT_LRLON - MapServer.ROOT_ULLON) / N;
+        double lat = ullat - lrlat;
+        double lon = lrron - ullon;
+        int height = (int) Math.round(lon / lonOffset);
+        int width = (int) Math.round(lat / latOffset);
+        int initialX = (int) Math.round((ullon - MapServer.ROOT_ULLON) / lonOffset);
+        int initialY = (int) Math.round((MapServer.ROOT_ULLAT - ullat) / latOffset);
+        String[][] grid = new String[width][height];
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
+                grid[i][j] = "d" + depth + "_x" + (j + initialX) + "_y" + (i + initialY) + ".png";
+            }
+        }
+        return grid;
+    }
+
+    private double getRasterLRLAT(double lrlat, int depth) {
+        double raster = MapServer.ROOT_LRLAT;
+        double offset = (MapServer.ROOT_ULLAT - MapServer.ROOT_LRLAT) / Math.pow(2, depth);
+        while (raster <= lrlat) {
+            raster += offset;
+        }
+        raster -= offset;
+        return raster;
+    }
+
+    private double getRasterULLAT(double ullat, int depth) {
+        double raster = MapServer.ROOT_ULLAT;
+        double offset = (MapServer.ROOT_ULLAT - MapServer.ROOT_LRLAT) / Math.pow(2, depth);
+        while (raster >= ullat) {
+            raster -= offset;
+        }
+        raster += offset;
+        return raster;
+    }
+
+    private double getRasterLRLON(double lrlon, int depth) {
+        double raster = MapServer.ROOT_LRLON;
+        double offset = (MapServer.ROOT_LRLON - MapServer.ROOT_ULLON) / Math.pow(2, depth);
+        while (raster >= lrlon) {
+            raster -= offset;
+        }
+        raster += offset;
+        return raster;
+    }
+
+    private double getRasterULLON(double ullon, int depth) {
+        double raster = MapServer.ROOT_ULLON;
+        double offset = (MapServer.ROOT_LRLON - MapServer.ROOT_ULLON) / Math.pow(2, depth);
+        while (raster <= ullon) {
+            raster += offset;
+        }
+        raster -= offset;
+        return raster;
     }
 
     private int getDepth(double boxLonDPP, double w, double h) {
@@ -63,7 +137,7 @@ public class Rasterer {
         double lrlon = MapServer.ROOT_LRLON;
         double ullon = MapServer.ROOT_ULLON;
         double lonDPP = calcLonDPP(lrlon, ullon, MapServer.TILE_SIZE);
-        while (lonDPP > boxLonDPP) {
+        while (lonDPP > boxLonDPP && depth < 7) {
             depth++;
             ullon = lrlon + (ullon - lrlon) / 2;
             lonDPP = calcLonDPP(lrlon, ullon, MapServer.TILE_SIZE);
@@ -88,7 +162,7 @@ public class Rasterer {
         if (ullon > lrlon) {
             return false;
         }
-        if (ullat > lrlat) {
+        if (ullat < lrlat) {
             return false;
         }
         if (ullon < MapServer.ROOT_ULLON) {
