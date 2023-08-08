3b8160163b1fba843d752d15fd2e25a9e9400598
teddynight <tanyuliang2@gmail.com>
2023-08-08 15:47:27 +0800

    lab14: implemented StrangeBitwiseGenerator

---
+++ lab14/lab14lib/StrangeBitwiseGenerator.java
@@ -0,0 +1,22 @@
+package lab14lib;
+
+public class StrangeBitwiseGenerator implements Generator {
+    private int period;
+    private int state;
+
+    public StrangeBitwiseGenerator(int period) {
+        this.period = period;
+        this.state = 0;
+    }
+
+    private double normalize(double s) {
+        return (s - period / 2) / (double) (period / 2);
+    }
+
+    @Override
+    public double next() {
+        state = (state + 1) % period;
+        int weirdState = state & (state >>> 3) % period;
+        return normalize(weirdState);
+    }
+}
